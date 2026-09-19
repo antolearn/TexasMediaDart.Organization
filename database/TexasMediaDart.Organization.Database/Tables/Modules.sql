46a173505ff89a81917e842ff5aca23d9e00a51b
Anto <antolearn@gmail.com>
2026-09-18 21:13:18 -0500

    Add module menu metadata and user entitlements

--- database/TexasMediaDart.Organization.Database/Tables/Modules.sql
+++ database/TexasMediaDart.Organization.Database/Tables/Modules.sql
@@ -5,11 +5,24 @@ CREATE TABLE [dbo].[Modules]
     [Name]        NVARCHAR(150) NOT NULL,
     [Description] NVARCHAR(500) NULL,
 
-    [IsActive]    BIT NOT NULL
+    -- Navigation metadata
+    [Route]        NVARCHAR(200) NULL,
+    [IconKey]      NVARCHAR(100) NULL,
+    [MenuGroup]    NVARCHAR(100) NULL,
+
+    [DisplayOrder] INT NOT NULL
+        CONSTRAINT [DF_Modules_DisplayOrder]
+        DEFAULT (0),
+
+    [ShowInMenu]   BIT NOT NULL
+        CONSTRAINT [DF_Modules_ShowInMenu]
+        DEFAULT (1),
+
+    [IsActive]     BIT NOT NULL
         CONSTRAINT [DF_Modules_IsActive]
         DEFAULT (1),
 
-    [CreatedUtc]  DATETIME2(7) NOT NULL
+    [CreatedUtc]   DATETIME2(7) NOT NULL
         CONSTRAINT [DF_Modules_CreatedUtc]
         DEFAULT (SYSUTCDATETIME()),
 
